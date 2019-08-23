d930b84855affe369e21fe700c27b96bb9325520
Stefanie Doll <stefie.doll@googlemail.com>
2019-08-23 16:52:48 +0200

    setup basic UI

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # AssemblyScript-Setup
-Initial AssemblyScript projects setup 
 
-Just an Assemblyscript starter project created with the Quick-Start guide that uses `yarn` instead of `npm`:
+An AssemblyScript starter project setup.
 
-https://docs.assemblyscript.org/quick-start
+Based initially on the Quick-Start guide on https://docs.assemblyscript.org/quick-start. 
 
+It uses:
+
+- `yarn` as a package manager
+- `webpack-dev-server` to provide a very basic UI during development
+
+
+## Install dependencies:
 ```
-// install dependencies
 yarn install
-
-// To build the WASM files
-yarn run build
 ```
+
+## Build the WASM files and run the webpack development server on `http://localhost:8080`
+```
+yarn run start
+```
+
+Now open http://localhost:8080 and you should see something like this:
+
+To edit the *AssemblyScript* code that is getting compiled to WebAssembly, edit the files in the `./assembly/` folder.
+To edit the *Frontend UI* code that interacts with the WebAssembly modules, edit the files in the `./src/` folder.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-// The entry file of your WebAssembly module.
+fetch('./build/optimized.wasm')
+  .then(response => {
+    if (response.ok){
+      return response.arrayBuffer();
+    } 
+    throw new Error(`Unable to fetch Web Assembly file.`);
+  })
+  .then(bytes => {
+    return WebAssembly.instantiate(bytes, {
+      env: {
+        abort: function() {}
+      }
+    })
+  })
+  .then(results => {
+    const instance = results.instance;
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
+    const a: number = 75;
+    const b: number = 36;
+
+    // Call the WASM add function 
+    const added = document.createElement('div')
+    added.textContent = `${a} + ${b} results in ${instance.exports.add(a,b)}`;
+    document.querySelector('main')!.appendChild(added)
+    console.log(`${a} + ${b} results in ${instance.exports.add(a,b)}`)
+
+     // Call the WASM square function 
+    const incremented = document.createElement('div')
+    incremented.textContent = `${a} squared results in ${instance.exports.square(a)}`;
+    document.querySelector('main')!.appendChild(incremented)
+    console.log(`${a} squared results in ${instance.exports.square(a)}`)
+  }).catch(console.error);
+
